ffcad57f33598d77416b38fc5dfa149dfe372210
arpitguptaji <124889523+arpitguptaji@users.noreply.github.com>
2023-02-09 18:11:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const movieList = document.querySelector("#movie-list");
+
+const movies = [
+  {
+    title: "The Shawshank Redemption",
+    image: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+  },
+  {
+    title: "The Godfather",
+    image: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+  },
+  {
+    title: "The Godfather: Part II",
+    image: "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTIt
